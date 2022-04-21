027c2033d64fdc5c399f13c39cd3d70e0dac0b77
Kakoluz <alberto_kakolukylla@hotmail.com>
2022-04-21 22:16:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# **Theo**
+
+## **About Theo**
+----------
+Theo is a bot that is meant to be added to groups and will respond your dumbass friend with a variety of usefull and original insults, while don't letting them forget that 4th Gen is bad and they should instead turn to the better 3rd Gen.
+
+The name comes from one of Asato Asato's novels 86 - Eighty Six. Theotto Rikka is quite the edgy boy, so we thought that him friendly badmouthing you and your friends is quite on point.
+
+## **Usage**
+-----------------
+Grab the prebuilt Binaries from the release tab. It's build using .Net Core 5 they should work on Mono.
+
+* **Windows**: Just provide a telegram bot token file and a list of words to say and pass them as arguments
+* **Linux/MacOs**: Download and install Mono, just add the prefix Mono before the .exe name and it should run exactly the same.
+  
+**NOTE**: Not tested on mono. Feedback is well recieved.
+
+-------------
+## **Building**
+-	**Windows**: Clone the repository and import it in Visual Studio 2019
+
+You will need to add these two dotnet packages: *Telegram.Bot*, *Telegram.Bot.Extensions.Polling*
+
+---------    
+## **Extra**
+
+If you want to improve on it's desing, go ahead, it was written a random afternoon while doing some memes. Still, if you find a bug or think that you could improve it, don't be afraid of contacting me on twitter (@kakoALB) or leaving a issue here.
+
+I want to thank Refia who gave me the idea of this bot after seeing my 10k+ lines of insults processed from twitter.
+
+Also a Spanish demo it's running in @BlondeEdgyBot (or should be, no stable hosting as of writing this README).
---
+++ Theo/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Theo
+{
+    class Program
+    {
+        static string[] lines;
+        static int insults = 0;
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                string token = System.IO.File.ReadAllText(args[0]);
+                lines = System.IO.File.ReadAllLines(args[1]);
+                foreach (string s in lines)
+                {
+                    ++insults;
+                }
+                while (true)
+                {
+                    try
+                    {
+                        runBotAsync(token).Wait();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Catched exception: " + e.Message + "\n\nRebooting Bot");
+                    }
+                }
+            }
+            Console.WriteLine("Invalid arguments");
+            Console.ReadLine();
+        }
+
+        static async System.Threading.Tasks.Task runBotAsync(string token)
+        {
+            Random ran = new Random();
+            var botClient = new TelegramBotClient(token);
+            using var cts = new CancellationTokenSource();
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { } // receive all update types
+            };
+            botClient.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken: cts.Token);
+
+            var me = await botClient.GetMeAsync();
+
+            Console.WriteLine($"@{me.Username} is fully started");
+            Console.ReadLine();
+
+            // Send cancellation request to stop bot
+            cts.Cancel();
+
+            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+            {
+                // Only process Message updates: https://core.telegram.org/bots/api#message
+                if (update.Type != UpdateType.Message)
+                    return;
+
+                // Only process text messages
+                if (update.Message!.Type != MessageType.Text)
+                    return;
+
+                var chatId = update.Message.Chat.Id;
+                var messageText = update.Message.Text;
+                var messageLower = messageText.ToLower();
+
+                if (messageLower.Contains("shinnoh") || messageLower.Contains("shinoh") || messageLower.Contains("shinohh") || messageLower.Contains("sinoh") || messageLower.Contains("sinou") || messageLower.Contains("sinno") || messageLower.Contains("shino") || messageLower.Contains("shinnou") || messageLower.Contains("shinno"))
+                {
+                    Message sentMessage = await botClient.SendTextMessageAsync(
+                        chatId: chatId,
+                        text: "Shinnofag\n\nhttps://www.youtube.com/watch?v=5Vf1G3vUJ5g&ab_channel=Jespoke",
+                        disableNotification: true,
+                        replyToMessageId: update.Message.MessageId,
+                        cancellationToken: cancellationToken);
+                }
+                else if (messageText.Contains("@BlondeEdgyBot") && (update.Message.Chat.Type == ChatType.Group || update.Message.Chat.Type == ChatType.Supergroup))
+                {
+                    if (update.Message.ReplyToMessage != null)
+                    {
+                        Message sentMessage = await botClient.SendTextMessageAsync(
+                            chatId: chatId,
+                            text: lines[ran.Next(insults)],
+                            disableNotification: true,
+                            replyToMessageId: update.Message.ReplyToMessage.MessageId,
+                            cancellationToken: cancellationToken);
+                    }
+                    else
+                    {
+                        Message sentMessage = await botClient.SendTextMessageAsync(
+                                chatId: chatId,
+                                text: lines[ran.Next(insults)],
+                                disableNotification: true,
+                                cancellationToken: cancellationToken);
+                    }
+                }
+                else if (update.Message.Chat.Type == ChatType.Private)
+                {
+                    Message sentMessage = await botClient.SendTextMessageAsync(
+                            chatId: chatId,
+                            text: lines[ran.Next(insults)],
+                            disableNotification: true,
+                            cancellationToken: cancellationToken);
+                }
+
+            }
+
+            Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+            {
+                var ErrorMessage = exception switch
+                {
+                    ApiRequestException apiRequestException
+                        => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                    _ => exception.ToString()
+                };
+
+                Console.WriteLine(ErrorMessage);
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
